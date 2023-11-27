4e6f5fb4583f121a981977a6cfa3f446bb4e2b4d
Lemois 1337 <lemois1337@protonmail.com>
2023-11-27 17:53:29 +0100

    Fixed MaxSompi constant to 4_961_000_000

--- util/amount_test.go
+++ util/amount_test.go
@@ -29,7 +29,7 @@ func TestAmountCreation(t *testing.T) {
 		},
 		{
 			name:     "max producible",
-			amount:   29e9,
+			amount:   4961e6,
 			valid:    true,
 			expected: Amount(constants.MaxSompi),
 		},
@@ -106,8 +106,8 @@ func TestAmountUnitConversions(t *testing.T) {
 			name:      "MKAS",
 			amount:    Amount(constants.MaxSompi),
 			unit:      AmountMegaKAS,
-			converted: 29000,
-			s:         "29000 MKAS",
+			converted: 4961,
+			s:         "4961 MKAS",
 		},
 		{
 			name:      "kKAS",
